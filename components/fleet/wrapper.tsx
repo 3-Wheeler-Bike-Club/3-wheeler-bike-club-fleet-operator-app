f76bf7fd59e909dd696df79b99c0d220a0a49a05
Tickether <tickether@gmail.com>
2025-11-08 11:49:14 +0000

    feat: Update operator and guarantor schemas, add KYC redirect
    Removed 'required' constraints from several fields in the Guarantor and Operator mongoose schemas to allow more flexible data entry. Moved 'phone' field in Operator schema and ensured it is required. In Wrapper component, added logic to check operator compliance and redirect to KYC page if not compliant.

--- components/fleet/wrapper.tsx
+++ components/fleet/wrapper.tsx
@@ -1,11 +1,48 @@
 "use client"
 
-
+import { useBlockNumber, useReadContract } from "wagmi"
+import { fleetOperatorBook } from "@/utils/constants/addresses"
+import { fleetOperatorBookAbi } from "@/utils/abis/fleetOperatorBook"
+import { useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
 import { Menu } from "@/components/top/menu"
+import { useRouter } from "next/navigation"
+import { usePrivy } from "@privy-io/react-auth"
 
 
 export function Wrapper() {
 
+    const { user } = usePrivy()
+    console.log(user)
+    console.log(user?.wallet?.address)
+    const address = user?.wallet?.address as `0x${string}`
+    
+    const router = useRouter()
+
+
+    const compliantQueryClient = useQueryClient()
+    
+    const { data: blockNumber } = useBlockNumber({ watch: true })  
+
+    const { data: compliant, isLoading: compliantLoading, queryKey: compliantQueryKey } = useReadContract({
+        address: fleetOperatorBook,
+        abi: fleetOperatorBookAbi,
+        functionName: "isOperatorCompliant",
+        args: [address!],
+    })
+    useEffect(() => { 
+        compliantQueryClient.invalidateQueries({ queryKey: compliantQueryKey }) 
+    }, [blockNumber, compliantQueryClient, compliantQueryKey]) 
+
+
+
+    useEffect(() => {
+        console.log(compliant)
+
+        if (compliant === false) {
+            router.replace("/kyc")
+        }
+    }, [compliant])
 
     return (
         <div className="flex flex-col h-full p-4 md:p-6 lg:p-8 w-full gap-6">
