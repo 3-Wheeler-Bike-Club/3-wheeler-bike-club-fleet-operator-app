3e35bb135d8785a02b224ca49360cb5a6174ee3f
Tickether <tickether@gmail.com>
2025-12-17 14:12:16 +0000

    feat: integrate payFleetOperatorReservationFee
    add function to pay reservation fee onchain, update contract address for fleetOperatorBook & add props to makeReservation component with basic ui with dummy figures

--- utils/constants/addresses.tsx
+++ utils/constants/addresses.tsx
@@ -3,4 +3,4 @@ export const USDT: `0x${string}` = "0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e";
 export const USDT_ADAPTER: `0x${string}` = "0x0E2A3e05bc9A16F5292A6170456A710cb89C6f72";
 export const fleetOrderBook: `0x${string}` = "0x5ea12A16346F5066715f34DE654e8CA3Ba5D07Bf";
 export const fleetOrderYield: `0x${string}` = "0x35c6D73288F1dbE9674DA02DD924d8be7b6bBfeA";
-export const fleetOperatorBook: `0x${string}` = "0x3C376F3129C35b704394C6C086737ed0A1021Ea5";
+export const fleetOperatorBook: `0x${string}` = "0x6D59bE3B32Ca55edC80C720D65572fa48417C0CF";
